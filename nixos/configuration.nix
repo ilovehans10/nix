9da57fcb406dec33630d426fbddf4aa120e2e062
Hans Larsson <hanandlia@gmail.com>
2025-07-05 14:13:35 -0500

    remove option that conflicts with flakes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -198,11 +198,6 @@
     };
   };
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
-
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
 }
