085b627a345ea266416827d274cac56f6f079b59
Hans Larsson <hanandlia@gmail.com>
2025-08-15 21:03:13 -0500

    add initial fix for cursor

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -41,6 +41,18 @@
   home = {
     username = "hans";
     homeDirectory = "/home/hans";
+    pointerCursor = {
+      name = "Dracula-cursors";
+      package = pkgs.dracula-theme;
+      size = 24;
+
+      gtk.enable = true;
+
+      x11 = {
+        enable = true;
+        defaultCursor = "Dracula-cursors";
+      };
+    };
   };
 
   # Add stuff for your user as you see fit:
