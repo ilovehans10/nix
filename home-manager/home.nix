c7137f247453b62007539244023a28bf59dfedb6
Hans Larsson <hanandlia@gmail.com>
2025-10-03 13:06:16 -0500

    add git commit signing with 1password

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -303,7 +303,15 @@
     userEmail = "hanandlia@gmail.com";
     userName = "Hans Larsson";
     extraConfig = {
-      host."*" = { "IdentityAgent" = " ~/.1password/agent.sock "; };
+      gpg = { format = "ssh"; };
+      "gpg \"ssh\"" = {
+        program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+      };
+      commit = { gpgsign = true; };
+      user = {
+        signingKey =
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0zfpzv87Do8FuepBA1r6PbibGI94dCS+STwv2B5WJN";
+      };
       merge.conflictStyle = "diff3";
       push.default = "current"; # Match modern push behavior
       color.ui = "auto";
