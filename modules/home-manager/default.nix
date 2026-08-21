81c1a189adb27e528f7a954dccf503014db2fbe8
Hans Larsson <hanandlia@gmail.com>
2026-08-21 10:41:02 -0500

    add gpg program and gpg agent
    Signed-off-by: Hans Larsson <hanandlia@gmail.com>

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,11 +1,12 @@
 # Add your reusable home-manager modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
 # These should be stuff you would like to share with others, not your personal configurations.
-{
+{pkgs, ...}: {
   imports = [
     ./desktop.nix
     ./git.nix
     ./lazygit.nix
     ./nvim
+    ./services
     ./ssh.nix
     ./tmux
     ./vicinae.nix
@@ -13,4 +14,5 @@
     ./yazi.nix
     ./zsh.nix
   ];
+  programs.gpg.enable = true;
 }
